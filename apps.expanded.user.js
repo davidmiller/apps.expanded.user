f8b7986606d49f5fc793165dbe60598b19dc9414
David Miller <il.livid.dream@gmail.com>
2009-10-04 10:47:02 +0100

    tidying etc

--- apps.expanded.user.js
+++ apps.expanded.user.js
@@ -1,6 +1,22 @@
 // Google Apps expanded
 // version 0.1
-// 2009-09-03
+// 2009-09-04
+//
+// Copyright 2009 David Miller
+// @url           http://www.deadpansincerity.com
+//
+// This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // --------------------------------------------------------------------
 //
@@ -13,6 +29,7 @@
 //
 // ==UserScript==
 // @name          Google Apps Expanded
+// @namespace     http://code.deadpansincerity.com
 // @description   Expands Google apps to include Google Reader
 // @include       *google*
 // ==/UserScript==
